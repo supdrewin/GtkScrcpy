291ed446c7c1b924c41a9ea7c4597e5e51ad7a74
Alvin <supdrewin@outlook.com>
2024-11-25 18:31:24 +0800

    Object memory management

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use gtk::{glib, Application, Button};
+use std::cell::Cell;
+use std::rc::Rc;
+
+use gtk::glib::clone;
+use gtk::{glib, Application, Button, Orientation};
 use gtk::{prelude::*, ApplicationWindow};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
@@ -12,22 +16,52 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Press me!")
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    let button_decrease = Button::builder()
+        .label("Decrease")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    button.connect_clicked(|button| {
-        button.set_label("Hello World!");
-    });
+    let number = Rc::new(Cell::new(0));
+
+    button_increase.connect_clicked(clone!(
+        #[weak]
+        number,
+        #[weak]
+        button_decrease,
+        move |_| {
+            number.set(number.get() + 1);
+            button_decrease.set_label(&number.get().to_string());
+        }
+    ));
+    button_decrease.connect_clicked(clone!(
+        #[weak]
+        button_increase,
+        move |_| {
+            number.set(number.get() - 1);
+            button_increase.set_label(&number.get().to_string());
+        }
+    ));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     window.present();
